f162e6cc027988168317b8a1160d804d96dfc96a
rubncio <99298741+rubncio@users.noreply.github.com>
2022-03-29 18:49:59 +0200

    Añadiendo informacion al fichro

--- Documents/NetBeansProjects/RepositorioNetbeans/src/main/java/NewMain.java
+++ Documents/NetBeansProjects/RepositorioNetbeans/src/main/java/NewMain.java
@@ -14,7 +14,7 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Manueel");
+        System.out.println("Hola Manueel, ¿Que tal?");
     }
     
 }
